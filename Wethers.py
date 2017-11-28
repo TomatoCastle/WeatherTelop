100c8b19ac1d0adbf51728835724182defe1f103
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-28 20:31:50 +0900

    [fix]ファイル名変更とメソッドの雛形追加できていなかったのを修正

--- Wethers.py
+++ Wethers.py
@@ -5,5 +5,22 @@ class Wehters:
     def __init__(self):
         pass
 
+    def get_wether(self):
+        return None
+
+    def get_wether_with_update(self):
+        return None
+
+    def get_wether_data_from_api(self):
+        return None
+
+    def update(self):
+        pass
+
+    def __eq__(self, other):
+        return None
+
+    def __str__(self):
+        return None
 
 
