aca73f2e50464707c244387c671f158bfc68b4cb
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-29 20:55:50 +0900

    [add] 文字列に変換するのを追加

--- OnePleaceWether.py
+++ OnePleaceWether.py
@@ -26,12 +26,12 @@ class  OnePleaceWether:
         today = list(filter(lambda n: n['dateLabel'] == '今日', self.__wether_api_dict['forecasts']))[0]
         rt['today'] = today['telop']
         if today['temperature']['max'] is None:
-            rt['todayMaxTmp'] = ''
+            rt['todayMaxTmp'] = '-'
         else:
             rt['todayMaxTmp'] = today['temperature']['max']['celsius']
 
         if today['temperature']['min'] is None:
-            rt['todayMinTmp'] = ''
+            rt['todayMinTmp'] = '-'
         else:
             rt['todayMinTmp'] = today['temperature']['min']['celsius']
 
@@ -39,12 +39,12 @@ class  OnePleaceWether:
         tomorrow = list(filter(lambda n: n['dateLabel'] == '明日', self.__wether_api_dict['forecasts']))[0]
         rt['tomorrow'] = tomorrow['telop']
         if tomorrow['temperature']['max'] is None:
-            rt['tomorrowMaxTmp'] = ''
+            rt['tomorrowMaxTmp'] = '-'
         else:
             rt['tomorrowMaxTmp'] = tomorrow['temperature']['max']['celsius']
 
         if tomorrow['temperature']['min'] is None:
-            rt['tomorrowMinTmp'] = ''
+            rt['tomorrowMinTmp'] = '-'
         else:
             rt['tomorrowMinTmp'] = tomorrow['temperature']['min']['celsius']
 
@@ -55,7 +55,8 @@ class  OnePleaceWether:
         return None
 
     def __str__(self):
-        return None
+        dic = self.get_wether()
+        return "【{}の今日の天気】　天気:{} 最高気温:{}℃  最低気温:{}℃  【{}の明日の天気】天気:{} 最高気温:{}℃  最低気温:{}℃ ".format(dic['location'], dic['today'], dic['todayMaxTmp'], dic['todayMinTmp'],dic['location'], dic['tomorrow'],dic['tomorrowMaxTmp'],dic['tomorrowMinTmp'])
 
     def check_uri_type(self, uri):
         if not isinstance(uri,str):
