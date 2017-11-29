9da0fbf93e833178d84f4da2eab90c443004fde4
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-29 18:25:03 +0900

    [add]webAPIからデータを取得するメソッド追加

--- Wethers.py
+++ Wethers.py
@@ -1,9 +1,10 @@
-import json
-import request
+import requests
 
-class Wehters:
-    def __init__(self,uri):
-        pass
+class Wethers:
+    def __init__(self, uri):
+        self.check_uri_type(uri)
+        self.uri = uri
+        self.update()
 
     def get_wether(self):
         return None
@@ -15,7 +16,7 @@ class Wehters:
         return None
 
     def update(self):
-        pass
+        self.__wether_api_dict = requests.get(self.uri).json()
 
     def __eq__(self, other):
         return None
@@ -23,6 +24,6 @@ class Wehters:
     def __str__(self):
         return None
 
-    def check_uri_type(self,uri):
-        pass
-
+    def check_uri_type(self, uri):
+        if not isinstance(uri,str):
+            raise TypeError('\"uri\"\'s type must be \"str\"')
