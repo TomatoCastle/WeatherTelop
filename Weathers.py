839c4429097ac09ed6460af0b1b49e0e3c61be5e
tomato <nomu.tomato.zyusu@gmail.com>
2017-12-05 22:47:53 +0900

    [add] Weathersの雛形ファイル追加

---
+++ Weathers.py
@@ -0,0 +1,23 @@
+import OnePleaceWether
+
+class Weathers:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return None
+
+    def add_weather(self):
+        pass
+
+    def get_weather(self):
+        return None
+
+    def get_weather(self, i):
+        return None
+
+    def __getitem__(self):
+        return None
+
+    def __eq__(self):
+        return None
